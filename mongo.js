d5a467e4a484bfba5262c6b63a4fc0b4b0e2058a
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-06-22 20:51:10 -0300

    First Commit

---
+++ mongo.js
@@ -0,0 +1,12 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/intro-mongo', (err, db) => {
+  const pessoas = db.collection('pessoas')
+  pessoas.insert({
+    name: 'John',
+    birth: '1985-08-04'
+  }, (err, res) => {
+    console.log(err, res)
+  })
+})
