c624db058718093e8731574b01b6f712f2495874
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-06-22 21:09:43 -0300

    Update and Remove

--- mongo.js
+++ mongo.js
@@ -12,7 +12,23 @@ MongoClient.connect('mongodb://localhost:27017/intro-mongo', (err, db) => {
   })
 
   const cursorPessoas = pessoas.find({})
+
   cursorPessoas.forEach(doc => {
     console.log(doc)
   }, () => console.log('Fim'))
+
+  pessoas.update(
+    {
+      _id: mongodb.ObjectID('60d276b1b3bfe82948984096')
+    },
+    {
+      $set: {
+        name: 'Paul',
+        birth: '1988-05-07'
+      }
+    }, (err, res) => console.log(err))
+
+  pessoas.remove({
+    _id: mongodb.ObjectID("60d27856343b5e32dc47e95c")
+  }, (err, res) => console.log(err))
 })
